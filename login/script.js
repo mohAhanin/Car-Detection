008b5c13e160b7acc02fce90c0840ec9a6337aae
Shahrad Gazni <gazni.shahrad@gmail.com>
2025-02-01 21:31:23 +0330

    Create script.js

---
+++ login/script.js
@@ -0,0 +1,16 @@
+var submit_button = document.getElementById('submit');
+var users = [['gazni.shahrad@gmail.com', 'smartprogrammer'], ['shahrad', 'smartprogrammer'], ['rezaei@sutech.ac.ir', '654321']]
+submit_button.addEventListener('click', function() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  users.foreach((value, index) => {
+    let _username = value[0];
+    let _password = value[1];
+    if(username == _username && password == _password){
+      alert('Correct!');
+    }
+    else{
+      alert('Username or Password is incorrect!');
+    }
+  });
+});
